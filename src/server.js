82d998b78834202ddfc9a02881cc4e9b4e700c2b
LeoAnders <leonardoanders15@gmail.com>
2024-11-24 16:31:19 -0300

    Corpo da requisição em JSON `Stream & Buffers`

--- src/server.js
+++ src/server.js
@@ -2,8 +2,20 @@ import http from 'http'
 
 const task = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/tasks') {
     return res
@@ -12,23 +24,17 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'PUT' && url === '/tasks') {
+    const { title, description } = req.body;
+
     task.push(
       {
         "id": 1,
-        "title": "Finalizar relatório semanal",
-        "description": "Concluir e enviar o relatório semanal de desempenho para o gerente.",
+        "title": title,
+        "description": description,
         "completed_at": null,
         "created_at": "2024-11-01T10:00:00Z",
         "updated_at": "2024-11-01T10:00:00Z"
       },
-      {
-        "id": 2,
-        "title": "Atualizar site da empresa",
-        "description": "Revisar as informações da página inicial e atualizar imagens.",
-        "completed_at": null,
-        "created_at": "2024-11-15T14:30:00Z",
-        "updated_at": "2024-11-18T09:00:00Z"
-      }
     )
 
     return res.writeHead(201).end()
