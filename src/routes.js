76f529c3160fc3e354c70f5fdaa19b3b3830ef1f
LeoAnders <leonardoanders15@gmail.com>
2024-11-27 18:52:12 -0300

    Criado `Regex` dos parâmetros juntamente com a rota `DELETE`

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js";
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database
+
+export const routes = [
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const tasks = database.select('tasks')
+
+      return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      return res.end()
+    }
+  }
+]
--- src/server.js
+++ src/server.js
@@ -1,41 +1,25 @@
 import http from 'http'
 import { json } from './middleware/json.js';
-import { Database } from "./database.js";
-import { randomUUID } from 'node:crypto'
-
-const database = new Database
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/tasks') {
-    const tasks = database.select('tasks')
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(tasks))
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    console.log(routeParams);
+
+    return route.handler(req, res)
   }
 
-  if (method === 'POST' && url === '/tasks') {
-    const { title, description } = req.body;
-
-    const task = {
-      id: randomUUID(),
-      title,
-      description,
-      completed_at: null,
-      created_at: new Date(),
-      updated_at: new Date()
-    }
-
-    database.insert('tasks', task)
-
-    return res.writeHead(201).end()
-  }
-
-  return res.writeHead(404).end()
+  return res.writeHead(404).end();
 })
 
 server.listen(3333)
